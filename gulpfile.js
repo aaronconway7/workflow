ca25ba81bbf01c6667d8f206c33779ae57b8acb7
aaronconway7 <aaronconway796@gmail.com>
2016-12-23 17:07:26 +0000

    Updated

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,6 @@ var uglify          = require('gulp-uglify');
 var prefix          = require('gulp-autoprefixer');
 var cssnano         = require('gulp-cssnano');
 var htmlmin         = require('gulp-htmlmin');
-var mainBowerFiles  = require('main-bower-files');
-var filter          = require('gulp-filter');
 var runSequence     = require('run-sequence');
 
 gulp.task('sass', function(){
@@ -69,35 +67,18 @@ gulp.task('docs', function() {
         .pipe(gulp.dest('dist/assets/docs'))
 });
 
-gulp.task('bower-css', function() {
-
-    var cssFilter = filter('**/*.css', {restore: true});
-
-    return gulp.src(mainBowerFiles())
-        .pipe(cssFilter)
-        .pipe(gulp.dest('app/css'))
-});
-
-gulp.task('bower-js', function() {
-
-    var jsFilter = filter('**/*.js', {restore: true});
-
-    return gulp.src(mainBowerFiles())
-        .pipe(jsFilter)
-        .pipe(gulp.dest('app/js'))
-});
-
 gulp.task('minify', ['html-min', 'uglify', 'css-nano', 'image-optimise']);
-
-gulp.task('bower', ['bower-css', 'bower-js']);
 
 gulp.task('transfer', ['fonts', 'docs'])
 
 gulp.task('jade-watch', ['jade'], reload);
 
-gulp.task('watch', ['browserSync', 'sass', 'jade'], function (){
+gulp.task('jquery-watch', ['uglify'], reload);
+
+gulp.task('live', ['browserSync', 'sass', 'jade'], function (){
     gulp.watch('app/sass/*.sass', ['sass']);
     gulp.watch('./app/jade/*.jade', ['jade-watch']);
+    gulp.watch('./app/js/*.js', ['jquery-watch']);
     // Other watchers
 });
 
@@ -113,8 +94,8 @@ gulp.task('compile', ['minify', 'transfer']);
 
 gulp.task('default', function () {
     runSequence(
-        ['jade', 'sass', 'bower'],
+        ['jade', 'sass'],
         'compile',
-        'watch'
+        'live'
     )
 });
